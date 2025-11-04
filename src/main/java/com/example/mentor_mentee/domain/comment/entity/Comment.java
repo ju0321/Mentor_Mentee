1869acea511f12fb357e39e5c46099fde84668bc
Jiwoo <ojing0116@skuniv.ac.kr>
2025-11-04 19:24:18 +0900

    :sparkles: Feat: FK 설정

--- src/main/java/com/example/mentor_mentee/domain/comment/entity/Comment.java
+++ src/main/java/com/example/mentor_mentee/domain/comment/entity/Comment.java
@@ -1,10 +1,14 @@
 package com.example.mentor_mentee.domain.comment.entity;
 
+import com.example.mentor_mentee.domain.post.entity.Post;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,8 +28,12 @@ public class Comment {
   @Column(name = "id")
   private Long id;
 
-  @Column(nullable = false)
+  @Column(name = "body", nullable = false, length = 300)
   private String body;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name="post_id", nullable = false)  //FK 생성
+  private Post post;
   //private String author;
 
 }
